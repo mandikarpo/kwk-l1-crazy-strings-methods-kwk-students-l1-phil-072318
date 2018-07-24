79219dde54c9c1f34b619c39cd42bad4fbc19f0d
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-24 14:45:44 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(a,b) 
+  
+  puts a.upcase.reverse + " " + b.tr("s","z").swapcase
+  
 end
+
+crazy_strings("Hello","Friends")
