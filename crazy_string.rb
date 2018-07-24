7339db65271ae6e3448586061a4a2b85154132b1
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-24 14:40:22 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,9 @@
 # write your method here
 
 def crazy_strings(a,b)
-  puts a.reverse.upcase + " " + b.tr("s","z").swapcase 
+  puts a.reverse.upcase 
+  puts " "
+  puts b.tr("s","z").swapcase 
   
 end
 
